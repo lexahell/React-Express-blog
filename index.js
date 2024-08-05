cd46dfd155b3dd7ba685470114811ce2542ad672
aleksey-grigorev <aleksey-grigorev7@mail.ru>
2024-08-05 14:28:48 +0300

    feat: add environment variables for deployment

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import { UserController, PostController } from './controllers/index.js';
 
 mongoose
   .connect(
-    'mongodb+srv://admin:wwwwww@cluster0.hzwtuyn.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0'
+    process.env.MONGODB_URI
   )
   .then(() => {
     console.log('DB ok');
@@ -78,7 +78,7 @@ app.patch(
   handleValidationErrors,
   PostController.update
 );
-app.listen(4444, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
   if (err) {
     return console.log(err);
   }
